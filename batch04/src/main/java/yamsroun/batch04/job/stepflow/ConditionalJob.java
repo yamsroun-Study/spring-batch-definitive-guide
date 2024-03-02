7c24a322912e1b83042afba7700dc44e8c3c3040
임재진(메가존) <mzjjlim@lgupluspartners.co.kr>
2024-03-02 22:20:50 +0900

    4장_ 잡과 스텝 이해하기 - 잡 종료하기 (1)

--- batch04/src/main/java/yamsroun/batch04/job/stepflow/ConditionalJob.java
+++ batch04/src/main/java/yamsroun/batch04/job/stepflow/ConditionalJob.java
@@ -25,8 +25,8 @@ public class ConditionalJob {
     @Bean
     public Tasklet passTasklet() {
         return (contribution, chunkContext) -> {
-            return RepeatStatus.FINISHED;
-            //throw new RuntimeException("This is a failure");
+            //return RepeatStatus.FINISHED;
+            throw new RuntimeException("This is a failure");
         };
     }
 
@@ -50,9 +50,8 @@ public class ConditionalJob {
     public Job job() {
         return jobBuilderFactory.get("conditionalJob")
             .start(firstStep())
-            .next(decider())
-            .from(decider()).on("FAILED").to(failureStep())
-            .from(decider()).on("*").to(successStep())
+            .on("FAILED").end()
+            .from(firstStep()).on("*").to(successStep())
             .end()
             .incrementer(new DailyJobTimestamper())
             .build();
