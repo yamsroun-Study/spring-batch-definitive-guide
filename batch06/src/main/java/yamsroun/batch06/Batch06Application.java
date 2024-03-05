a6de3a216ac05105af9f27e8c2be31f5fbc0dda4
임재진(메가존) <mzjjlim@lgupluspartners.co.kr>
2024-03-05 20:38:09 +0900

    6장_ 잡 실행하기 - 프로젝트 생성

---
+++ batch06/src/main/java/yamsroun/batch06/Batch06Application.java
@@ -0,0 +1,13 @@
+package yamsroun.batch06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Batch06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Batch06Application.class, args);
+    }
+
+}
---
+++ batch06/src/test/java/yamsroun/batch06/Batch06ApplicationTests.java
@@ -0,0 +1,13 @@
+package yamsroun.batch06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Batch06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
