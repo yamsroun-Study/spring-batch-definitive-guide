714b904765abfa034820ae4fe44169ae2240b742
임재진(메가존) <mzjjlim@lgupluspartners.co.kr>
2024-03-05 23:53:59 +0900

    6장_ 잡 실행하기 - REST 방식으로 잡 실행하기 (2)

--- batch06/src/main/java/yamsroun/batch06/controller/BatchController.java
+++ batch06/src/main/java/yamsroun/batch06/controller/BatchController.java
@@ -2,6 +2,7 @@ package yamsroun.batch06.controller;
 
 import lombok.*;
 import org.springframework.batch.core.*;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.*;
@@ -12,15 +13,20 @@ import java.util.Properties;
 @RestController
 public class BatchController {
 
-    private final JobLauncher jobLauncher;
     private final ApplicationContext context;
+    private final JobLauncher jobLauncher;
+    private final JobExplorer jobExplorer;
 
     @PostMapping(path = "/run")
     public ExitStatus runJob(
         @RequestBody JobLaunceRequest request
     ) throws Exception {
         Job job = context.getBean(request.getName(), Job.class);
-        return jobLauncher.run(job, request.getJobParameters())
+        JobParameters jobParameters =
+            new JobParametersBuilder(request.getJobParameters(), jobExplorer)
+                .getNextJobParameters(job)
+                .toJobParameters();
+        return jobLauncher.run(job, jobParameters)
             .getExitStatus();
     }
 
